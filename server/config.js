b8c7a6de66a8959de8c66fb1b9bcf5b5594a5b4c
crollins <crollins@DRWHoldings.com>
2016-10-27 14:39:24 -0500

    [cr] commit

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://react:react@ds031257.mlab.com:31257/expense',
   port: process.env.PORT || 8000,
 };
 
