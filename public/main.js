c4f34c8503b7d94b4151add4195099977776d811
Coleman <crollins199@gmail.com>
2017-02-09 10:53:42 -0600

    [cr] added simple CRUD from tutorial

---
+++ public/main.js
@@ -0,0 +1,39 @@
+const update = document.querySelector('#update');
+const del = document.querySelector('#delete');
+
+update.addEventListener('click', () => {
+    fetch('quotes', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            'name': 'Darth Vader',
+            'quote': 'I find your lack of faith disturbing.'
+        })
+    })
+    .then(res => {
+        if (res.ok) return res.json()
+    })
+    .then(data => {
+        console.log(data)
+        window.location.reload(true)
+    })
+});
+
+del.addEventListener('click', () => {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Darth Vader'
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  }).
+  then(data => {
+    console.log(data)
+    window.location.reload(true)
+  })
+})
