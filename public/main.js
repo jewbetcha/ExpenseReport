9b6ba7f247eaa5902ef7c21e757bb9460767d578
Coleman <crollins199@gmail.com>
2017-02-09 15:40:33 -0600

    [cr] added check for no expenses listed

--- public/main.js
+++ public/main.js
@@ -1,27 +1,10 @@
+// Styles
 import './base.scss'
 import './main.scss';
 
 
-// const update = document.querySelector('#update');
+// Give all delete buttons action to delete respective entry
 const del = document.querySelectorAll('.delete');
-
-// update.addEventListener('click', () => {
-//     fetch('quotes', {
-//         method: 'put',
-//         headers: {'Content-Type': 'application/json'},
-//         body: JSON.stringify({
-//             'name': 'Darth Vader',
-//             'quote': 'I find your lack of faith disturbing.'
-//         })
-//     })
-//     .then(res => {
-//         if (res.ok) return res.json()
-//     })
-//     .then(data => {
-//         console.log(data)
-//         window.location.reload(true)
-//     })
-// });
 
 for (let i of del) {
   const name = i.getAttribute('name');
@@ -44,3 +27,22 @@ for (let i of del) {
     })
   })
 }
+
+// update.addEventListener('click', () => {
+//     fetch('quotes', {
+//         method: 'put',
+//         headers: {'Content-Type': 'application/json'},
+//         body: JSON.stringify({
+//             'name': 'Darth Vader',
+//             'quote': 'I find your lack of faith disturbing.'
+//         })
+//     })
+//     .then(res => {
+//         if (res.ok) return res.json()
+//     })
+//     .then(data => {
+//         console.log(data)
+//         window.location.reload(true)
+//     })
+// });
+
